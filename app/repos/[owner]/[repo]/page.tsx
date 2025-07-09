5fc0c7a7a80437ac778023599387b4c8b1e766cb
DESKTOP-5670IME\chii8 <chii81020@gmail.com>
2025-07-10 02:05:36 +0900

    fix: 型定義の修正

--- app/repos/[owner]/[repo]/page.tsx
+++ app/repos/[owner]/[repo]/page.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 interface Props {
-  params: { owner: string; repo: string };
+  params: Promise<{ owner: string; repo: string }>;
 }
 
 export default async function RepoDetail({ params }: Props) {
